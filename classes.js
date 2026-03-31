6d3f21822cf9a7e4937c075c02d5d04bc578368e
Anna Moore <annathekarategirl@gmail.com>
2026-03-31 13:18:20 -0400

    f

---
+++ classes.js
@@ -0,0 +1,20 @@
+const image=document.getElementById("image")
+const text1=document.getElementById("text1")
+const text2=document.getElementById("text2")
+const buttonDiv=document.getElementById("buttons")
+
+class Scene{
+    constructor(image_path,text1,text2)
+{
+    this.image_path=image_path
+    this.text1=text1
+    this.text2=text2
+}
+renderScene(){
+    image.src=this.image_path
+    text1.innerText=this.text1
+    text2.innerHTML=this.text2;
+
+}
+}
+let introScene=new Scene("image2.webp","enstasrs","rio")
